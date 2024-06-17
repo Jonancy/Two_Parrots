97af11d53cdb59938b15a7256a69c6656f67e67a
Samir Gautam <96002032+nobody4o4@users.noreply.github.com>
2024-06-17 13:16:59 +0545

    routes init

--- client/src/routes/admin.routes.ts
+++ client/src/routes/admin.routes.ts
@@ -2,12 +2,6 @@
 import Dashboard from "@/pages/admin/dashboard/index.dashboard";
 import Request from "@/pages/admin/Request";
 import { lazy } from "react";
-
-
-
-
-
-
 export const adminRoutes = [
     {
         id: "dashboard",
